c7a741452084e250907a1dab0817b6ee99437e51
chenjianhua <jianhua.chen@szkunton.com>
2020-11-04 19:02:07 +0800

    抖音下载

--- python/request/douyin.py
+++ python/request/douyin.py
@@ -1,4 +1,3 @@
-
 import requests
 import json
 import re
@@ -6,22 +5,20 @@ import os
 import sys
 import time
 from urllib.parse import urlparse
+from urllib.parse import parse_qs
 from contextlib import closing
 
 
 class DouYin(object):
     def __init__(self):
         self.headers = {
-            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+            'accept': 'application/json,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
             'accept-encoding': 'gzip, deflate, br',
             'accept-language': 'zh-CN,zh;q=0.9',
             'cache-control': 'max-age=0',
             'upgrade-insecure-requests': '1',
-            'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1'
+            'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1'
         }
-
-        self.domain = ['www.douyin.com', 'v.douyin.com', 'www.snssdk.com',
-                       'www.amemv.com', 'www.iesdouyin.com', 'aweme.snssdk.com']
 
     def shareViedo(self, shareUrl, baseDir):
         self.baseDir = baseDir
@@ -29,15 +26,51 @@ class DouYin(object):
         self.share_url = self.getLocation(shareUrl)
         print("request: "+self.share_url)
         share_url_parse = urlparse(self.share_url)
-        # 解析视频Id
-        videoId = list(filter(None, share_url_parse.path.split("/")))[2]
-        # 获取视频信息
-        videoInfo = self.getVideoInfo(videoId)
-        # 将地址里的playwm改为play就是无水印播放地址了,头部user-agent需要手机user-agent
-        videoInfo["mp4Url"] = videoInfo["mp4Url"].replace("playwm", "play")
-        print(videoInfo)
-        self.downloadVideoAndAudio(videoInfo)
+        shareUrlPath = list(filter(None, share_url_parse.path.split("/")))
+        if "user" == shareUrlPath[1]:
+            share_url_query = parse_qs(share_url_parse.query, True)
+            if share_url_query.get("sec_uid"):
+                sec_uid = share_url_query.get("sec_uid")[0]
+                userInfo = self.getUserInfo(sec_uid)
+                print(userInfo)
 
+        else:
+            # 解析视频Id
+            videoId = shareUrlPath[2]
+            # 获取视频信息
+            videoInfo = self.getVideoInfo(videoId)
+            # 将地址里的playwm改为play就是无水印播放地址了,头部user-agent需要手机user-agent
+            videoInfo["mp4Url"] = videoInfo["mp4Url"].replace("playwm", "play")
+            print(videoInfo)
+            self.downloadVideoAndAudio(videoInfo)
+
+    def getUserInfo(self, sec_uid):
+        # 获取用户信息
+        userInfoUrl = "https://www.iesdouyin.com/web/api/v2/user/info/?sec_uid="+sec_uid
+        print("request: "+userInfoUrl)
+        infoResp = requests.get(userInfoUrl)
+        userInfo = {}
+        respJson = infoResp.json()
+        userInfo["nickname"] = respJson["user_info"]["nickname"]
+        userInfo["signature"] = respJson["user_info"]["signature"]
+        userInfo["uid"] = respJson["user_info"]["uid"]
+        # 获取用户发布的视频
+        print(userInfo["uid"])
+        signature = self.generateSignature(userInfo["uid"])
+        print("signature: "+signature)
+        userVideoUrl = "https://www.iesdouyin.com/web/api/v2/aweme/post/?sec_uid=" + \
+            sec_uid + "&count=21&max_cursor=0&aid=1128&_signature="+signature
+
+        print("request: "+userVideoUrl)
+        userVideoResp = requests.get(userVideoUrl)
+        print(json.dumps(userVideoResp.json(), sort_keys=True, indent=4))
+        return userInfo
+
+    @staticmethod
+    def generateSignature(value):
+        p = os.popen('node fuck-byted-acrawler.js %s' % value)
+        return (p.readlines()[0]).strip()
+    
     def getVideoInfo(self, videoId):
         infoUrl = "https://www.iesdouyin.com/web/api/v2/aweme/iteminfo/?item_ids="+videoId
         print("request: "+infoUrl)
@@ -60,7 +93,7 @@ class DouYin(object):
             print("没有获取到跳转地址")
             return ""
 
-    def downloadVideoAndAudio(self,videoInfo):
+    def downloadVideoAndAudio(self, videoInfo):
         path = os.path.join(self.baseDir, videoInfo["nickname"])
         # 目录不存在，就新建一个
         if not os.path.exists(path):
@@ -77,9 +110,12 @@ class DouYin(object):
             f2.close()
             print("===>音频和视频下载完成")
 
+
 if __name__ == '__main__':
-    # 分享链接
-    shareUrl = "https://v.douyin.com/JmenG65/"
+    # 分享链接,视频
+    # shareUrl = "https://v.douyin.com/JmenG65/"
+    # 用户分享
+    shareUrl = "https://v.douyin.com/JmRvpUD/"
     baseDir = "C:\\Users\\Administrator\\Desktop\\douyin"
     douyin = DouYin()
     douyin.shareViedo(shareUrl, baseDir)

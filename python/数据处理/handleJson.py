d47094ae3f01b03123498c4e1c8b848a2b610763
chenjianhua <2290910211@qq.com>
2021-05-09 23:36:45 +0800

    pandas

--- python/数据处理/handleJson.py
+++ python/数据处理/handleJson.py
@@ -1,4 +1,6 @@
 import pandas
+from string import Template
+
 jsonFile = "data.json"
 
 df = pandas.read_json(jsonFile)
@@ -9,5 +11,9 @@ print(dictData)
 for row in df.itertuples():
     print(row.bookName)
 
+hellpTemplate = Template('hello $name $world')
+
 for rowDict in dictData:
-    print(rowDict)
+    resultStr = hellpTemplate.safe_substitute(name=rowDict["bookName"])
+    print(resultStr)
+
